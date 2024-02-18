baba04be18da9ea94afcff17e32cf0a907b20f55
osipov <osipov27032002@mail.ru>
2024-02-18 18:20:06 +0400

    Add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import {useState} from 'react'
 import Login from "./components/Login.jsx";
+import Client from "./pages/Client.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Admin from "./pages/Admin.jsx";
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <Login></Login>
-    )
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login/>}></Route>
+                    <Route path="/client" element={<Client/>}></Route>
+                    <Route path="/admin" element={<Admin/>}></Route>
+
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Client.jsx
@@ -0,0 +1,11 @@
+const Client = () => {
+    return (
+        <>
+            <h1>
+                Страница клиента
+            </h1>
+        </>
+    )
+}
+
+export default Client;
