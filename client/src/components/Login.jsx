eb07982b650f7b1afdb2553b05fb83c6061ea55b
osipov <osipov27032002@mail.ru>
2024-02-18 14:44:03 +0400

    Add login useState function

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,13 +4,24 @@ import '../assets/css/main.css'
 import '../assets/css/util.css'
 import '../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css'
 import '../assets/fonts/iconic/css/material-design-iconic-font.min.css'
+import {useState} from "react";
+
 const Login = () => {
+    const [userData, setUserData] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(userData)
+    }
     return (
         <>
             <div className="limiter">
                 <div className="container-login100" style={{backgroundImage: "url(" + background + ")"}}>
                     <div className="wrap-login100">
-                        <form className="login100-form validate-form">
+                        <form className="login100-form validate-form" onSubmit={handleSubmit}>
 					<span className="login100-form-logo">
                         <img src={logo} alt=""/>
 					</span>
@@ -20,19 +31,21 @@ const Login = () => {
 					</span>
 
                             <div className="wrap-input100 validate-input" data-validate="Enter username">
-                                <input className="input100" type="text" name="username" placeholder="Логин / Login"/>
+                                <input className="input100" type="text" name="username" placeholder="Логин / Login"
+                                       onChange={e => setUserData({...userData, username: e.target.value})}/>
                                 <span className="focus-input100" data-placeholder="&#xf207;"></span>
                             </div>
 
                             <div className="wrap-input100 validate-input" data-validate="Enter password">
                                 <input className="input100" type="password" name="pass"
-                                       placeholder="Пароль / Password"/>
+                                       placeholder="Пароль / Password"
+                                       onChange={e => setUserData({...userData, password: e.target.value})}/>
                                 <span className="focus-input100" data-placeholder="&#xf191;"></span>
                             </div>
 
 
                             <div className="container-login100-form-btn">
-                                <button className="login100-form-btn">
+                                <button className="login100-form-btn" type="submit">
                                     Войти
                                 </button>
                             </div>
